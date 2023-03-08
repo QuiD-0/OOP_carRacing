4f841c27c706271026ee5bd7f09e9689d8f7f98e
quid-0 <wodnd101@naver.com>
2023-03-08 22:44:18 +0900

    init

---
+++ src/main/java/com/quid/carracing/CarRacingApplication.java
@@ -0,0 +1,13 @@
+package com.quid.carracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRacingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRacingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quid/carracing/CarRacingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quid.carracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRacingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
