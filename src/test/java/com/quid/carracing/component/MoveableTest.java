b49abb0324c746e41d994a11a50b14a11ef48edd
QuiD-0 <wodnd101@naver.com>
2023-03-14 17:47:34 +0900

    Display Name 추가

--- src/test/java/com/quid/carracing/component/MoveableTest.java
+++ src/test/java/com/quid/carracing/component/MoveableTest.java
@@ -1,11 +1,13 @@
 package com.quid.carracing.component;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MoveableTest {
 
     @Test
+    @DisplayName("이동 가능한지 여부")
     public void isMoveTest() {
         Assertions.assertDoesNotThrow(Moveable::isMove);
     }
