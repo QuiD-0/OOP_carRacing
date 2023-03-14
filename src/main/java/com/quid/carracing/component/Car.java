437867a7fc62447facf0fbcb9acc7a1ecbacecf5
quid-x <quid@hanpass.com>
2023-03-14 17:41:17 +0900

    car 생성, 이동

--- src/main/java/com/quid/carracing/component/Car.java
+++ src/main/java/com/quid/carracing/component/Car.java
@@ -3,6 +3,7 @@ package com.quid.carracing.component;
 import java.util.List;
 
 public class Car {
+
     private final String name;
     private int position = 0;
 
@@ -12,5 +13,23 @@ public class Car {
 
     public static List<Car> create(List<String> names) {
         return names.stream().map(Car::new).toList();
+    }
+
+    public void move() {
+        if(Moveable.isMove()) {
+            position += 1;
+        }
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public int position() {
+        return position;
+    }
+
+    public boolean isSamePosition(int maxPosition) {
+        return this.position == maxPosition;
     }
 }
--- src/test/java/com/quid/carracing/component/CarTest.java
+++ src/test/java/com/quid/carracing/component/CarTest.java
@@ -2,16 +2,28 @@ package com.quid.carracing.component;
 
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CarTest {
 
     @Test
+    @DisplayName("자동차 생성")
     public void makeCarTest() {
         List<String> names = List.of("jay", "juniq", "junny", "jureung");
 
         List<Car> cars = Car.create(names);
 
         Assertions.assertEquals(cars.size(), 4);
+    }
+
+    @Test
+    @DisplayName("자동차 이동")
+    public void moveCarTest() {
+        List<String> names = List.of("jay", "juniq", "junny", "jureung");
+        List<Car> cars = Car.create(names);
+        Car car = cars.get(0);
+
+        Assertions.assertDoesNotThrow(car::move);
     }
 }
